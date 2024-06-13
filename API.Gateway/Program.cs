af6dbaed3d0f51375ec69382a2167469e5d3fdbb
Lex de Kort <2232771+lex-kort@users.noreply.github.com>
2024-06-13 03:07:16 +0200

    Add session handler endpoint

--- API.Gateway/Program.cs
+++ API.Gateway/Program.cs
@@ -4,19 +4,25 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration)
     .AddCacheManager(x =>
     {
         x.WithDictionaryHandle();
     });
 
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+
 var app = builder.Build();
+
+//app.UseRouting();
+//app.UseEndpoints(_ => { });
 
 //app.UseHttpsRedirection();
 
 //app.UseAuthentication();
 //app.UseAuthorization();
+app.UseWebSockets();
+app.UseOcelot().Wait();
 
-await app.UseOcelot();
 await app.RunAsync();
