38be6e5d8eb509b2f2a50f9a2e4a1c01498f30e7
Lex de Kort <2232771+lex-kort@users.noreply.github.com>
2024-06-19 00:33:25 +0200

    Temporary reversal for debugging

--- API.Gateway/Program.cs
+++ API.Gateway/Program.cs
@@ -5,15 +5,22 @@ using Ocelot.Provider.Kubernetes;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Services.AddOcelot(builder.Configuration)
+//    .AddKubernetes()
+//    .AddCacheManager(x =>
+//    {
+//        x.WithDictionaryHandle();
+//    });
+
+//builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
 builder.Services.AddOcelot(builder.Configuration)
-    .AddKubernetes()
     .AddCacheManager(x =>
     {
         x.WithDictionaryHandle();
     });
 
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-
+builder.Configuration.AddJsonFile($"ocelot.Development.json", optional: true, reloadOnChange: true);
 
 var app = builder.Build();
 
